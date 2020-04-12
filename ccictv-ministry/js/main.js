19ede50962b12bbeb31c9caca23ed94f853ecaf2
EricZh2020 <ericzh@gmail.com>
2020-04-12 16:00:15 -0700

    for fixing ms edge left site menu issue

--- ccictv-ministry/js/main.js
+++ ccictv-ministry/js/main.js
@@ -26,12 +26,11 @@
 	};
 
 	var fullHeight = function() {
-
 		if ( !isMobile.any() ) {
-			$('.js-fullheight').css('height', $(window).height());
-			$(window).resize(function(){
-				$('.js-fullheight').css('height', $(window).height());
-			});
+			$('.js-fullheight').css('height', $('#colorlib-main').height());
+			 $(window).resize(function(){
+				$('.js-fullheight').css('height', $('#colorlib-main').height());
+			 });
 		}
 
 	};
